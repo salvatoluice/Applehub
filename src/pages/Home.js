92093d3eee9665aec5c796bfd96ab13a0d663cac
salvatoluice <salvatoluice5@gmail.com>
2023-05-08 22:52:43 +0300

    Add contact section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -315,7 +315,7 @@ const Home = () => {
       <Container class1="blog-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
-            <h3 className="section-heading">Our Latest Blogs</h3>
+            <h3 className="section-heading">Blog</h3>
           </div>
         </div>
         <div className="row">
