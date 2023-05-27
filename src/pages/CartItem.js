da37859ec81f1e2295b8bd98a5a23c9703e6c796
salvatoluice <salvatoluice5@gmail.com>
2023-05-27 20:12:40 +0300

    Add cart functionality

--- src/pages/CartItem.js
+++ src/pages/CartItem.js
@@ -1,6 +1,37 @@
 import React from 'react'
 
 const CartItem = () => {
+    const [quantity, setQuantity] = useState(1);
+  const { cartItem, setCartItem } = useContext(CartContext);
+
+  const increase = () => {
+    if (quantity >= 1) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const calcPrice = (quantity, item) => {
+    return quantity * item;
+  };
+
+  const [deleteItem, setDeleteItem] = useState(cartItem);
+
+  const removeFromCart = (id) => {
+    const updateCart = cartItem.filter((item) => item.id !== id);
+    setDeleteItem(updateCart);
+    const json = JSON.stringify(cartItem.id);
+    localStorage.removeItem("cartItem", json);
+  };
+
+  useEffect(() => {
+    setCartItem(deleteItem);
+  }, [deleteItem, setCartItem]);
   return (
     <div>
       
