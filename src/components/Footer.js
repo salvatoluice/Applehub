e7aa4ab5edaea3519ce38d19d78a931ab9c5d14d
salvatoluice <salvatoluice5@gmail.com>
2023-05-27 20:03:28 +0300

    Footer restructure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -98,10 +98,11 @@ const Footer = () => {
             <div className="col-2">
               <h4 className="text-white mb-4">Quick Links</h4>
               <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Phones</Link>
                 <Link className="text-white py-2 mb-1">Laptops</Link>
                 <Link className="text-white py-2 mb-1">Headphones</Link>
                 <Link className="text-white py-2 mb-1">Tablets</Link>
-                <Link className="text-white py-2 mb-1">Watch</Link>
+                <Link className="text-white py-2 mb-1">Watches</Link>
               </div>
             </div>
           </div>
