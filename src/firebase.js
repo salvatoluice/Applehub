7bfea4844b78a78786b7208cb7f34eb631fd679d
salvatoluice <salvatoluice5@gmail.com>
2023-05-28 20:19:44 +0300

    User auth using firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD4agorTgieP19ZrrAJWXU9cSbVSXngT5Y",
+    authDomain: "applehub-74596.firebaseapp.com",
+    projectId: "applehub-74596",
+    storageBucket: "applehub-74596.appspot.com",
+    messagingSenderId: "491654100481",
+    appId: "1:491654100481:web:644f73463a94269929b200",
+    measurementId: "G-Q8919ZS26C"
+  };
