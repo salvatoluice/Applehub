8cb29dc426677c837d0392bb5342c9f97f9a0f87
salvatoluice <salvatoluice5@gmail.com>
2023-05-28 20:40:40 +0300

    Firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,25 @@
+import { initializeApp } from "firebase/app";
+
+import { 
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword, 
+    sendPasswordResetEmail, 
+    signOut  
+    } from "firebase/auth";
+
+import { 
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+  } from "firebase/firestore";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyD4agorTgieP19ZrrAJWXU9cSbVSXngT5Y",
